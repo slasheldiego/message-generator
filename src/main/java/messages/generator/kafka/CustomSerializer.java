67819eed646ee9ca41075180c59c2863dafb404c
Diego Armando Benavides Vidal <slasheldiego@gmail.com>
2021-03-15 12:45:07 -0500

    Add a kafka producer generator messages 2

---
+++ src/main/java/messages/generator/kafka/CustomSerializer.java
@@ -0,0 +1,35 @@
+package messages.generator.kafka;
+
+import java.util.Map;
+import org.apache.kafka.common.serialization.Serializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import messages.generator.entities.Event;
+
+public class CustomSerializer implements Serializer<Event> {
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+
+    }
+
+    @Override
+    public byte[] serialize(String topic, Event data) {
+        byte[] retVal = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+        retVal = objectMapper.writeValueAsString(data).getBytes();
+        } catch (Exception exception) {
+        System.out.println("Error in serializing object"+ data);
+        }
+        return retVal;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+}
