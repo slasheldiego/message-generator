1478f5bdee9283c692c5ff460b2fb658424afa7a
Diego Armando Benavides Vidal <slasheldiego@gmail.com>
2021-08-15 02:55:23 -0500

    add kafka schema registry configuration

--- src/main/java/messages/generator/kafka/ProducerCreator.java
+++ src/main/java/messages/generator/kafka/ProducerCreator.java
@@ -2,6 +2,10 @@ package messages.generator.kafka;
 
 import java.util.Properties;
 
+import com.amazonaws.services.schemaregistry.serializers.avro.AWSKafkaAvroSerializer;
+import com.amazonaws.services.schemaregistry.utils.AWSSchemaRegistryConstants;
+import com.amazonaws.services.schemaregistry.utils.AvroRecordType;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -17,9 +21,17 @@ public class ProducerCreator {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.KAFKA_BROKERS);
         props.put(ProducerConfig.CLIENT_ID_CONFIG, IKafkaConstants.CLIENT_ID);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        //props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        //props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AWSKafkaAvroSerializer.class.getName());
         //props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class.getName());
+        props.put(AWSSchemaRegistryConstants.AWS_REGION, "us-east-1");
+        props.put(AWSSchemaRegistryConstants.AVRO_RECORD_TYPE, AvroRecordType.SPECIFIC_RECORD.getName());
+        props.put(AWSSchemaRegistryConstants.SCHEMA_AUTO_REGISTRATION_SETTING, "true");
+        props.put(AWSSchemaRegistryConstants.REGISTRY_NAME, "registry_name");
+        props.put(AWSSchemaRegistryConstants.SCHEMA_NAME, "schema_name");
+        props.put(AWSSchemaRegistryConstants.DESCRIPTION, "My schema name test");
         props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, IKafkaConstants.SECURITY_PROTOCOL_CONFIG);
         props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, IKafkaConstants.SSL_TRUSTSTORE_LOCATION_CONFIG);
         return new KafkaProducer<>(props);
